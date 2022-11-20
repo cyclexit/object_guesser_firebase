34b93e9f2f09f403723faf60c6df20aca2448329
cyclexit <cyclexit@outlook.com>
2022-11-19 22:44:32 -0500

    Update scripts

---
+++ images/upload_images.js
@@ -0,0 +1,2 @@
+const fs = require('fs');
+const admin = require('firebase-admin');
--- labels/upload_labels.js
+++ labels/upload_labels.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
+const admin = require('firebase-admin');
 
 // NOTE: `parent_id` and `root_id` will be replaced by hash id from firebase in the end
 //        and `id` will be added as the firebase hash id
 const rawData = fs.readFileSync('labels.json');
 const labels = JSON.parse(rawData);
 
-const admin = require('firebase-admin');
 admin.initializeApp({
     credential: admin.credential.cert(require('../credentials.json'))
 })
