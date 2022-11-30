89c2b5de6558fd25f0ff5f292b966a76969cf35c
cyclexit <cyclexit@outlook.com>
2022-11-30 11:41:59 -0500

    Successfully get the label counter

--- quizzes/selection/generate_selection_quizzes.js
+++ quizzes/selection/generate_selection_quizzes.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const admin = require('firebase-admin');
-const { assert } = require('console');
 
 const LABELS = "labels";
 const IMAGE_LABEL_RECORDS = "image_label_records";
@@ -22,15 +21,22 @@ const getData = async() => {
     const labelSnapshot = await labelsCollection.get();
     const imageSnapshot = await imagesCollection.get();
 
+    // {image_id: label_id}
+    var imageLabelRecords = {};
+    imageLabelRecordSnapshot.docs.forEach(doc => {
+        const data = doc.data();
+        imageLabelRecords[data["image_id"]] = data["label_id"];
+    });
     var labels = {};
     labelSnapshot.docs.forEach(doc => {
-        data = doc.data();
+        const data = doc.data();
         labels[data["id"]] = data;
-    })
+    });
+
     return {
-        "imageLabelRecords": imageLabelRecordSnapshot.docs.map(doc => doc.data()),
-        "labels": labels,
         "images": imageSnapshot.docs.map(doc => doc.data()),
+        "imageLabelRecords": imageLabelRecords,
+        "labels": labels,
     };
 }
 
@@ -42,22 +48,47 @@ const getNineImages = (images) => {
     return nineImages;
 }
 
-const generateQuizzes = async(nineImages) => {
+const getDisplayLabel = (nineImages, imageLabelRecords, labels) => {
+    var labelCounter = {};
+    for (const img of nineImages) {
+        const imgLabelId = imageLabelRecords[img["id"]];
 
+        var curLabelId = imgLabelId;
+        // console.log(curLabelId);
+        while (true) {
+            if (Object.keys(labelCounter).includes(curLabelId)) {
+                labelCounter[curLabelId].push(img["id"]);
+            } else {
+                labelCounter[curLabelId] = [img["id"]];
+            }
+            curLabelId = labels[curLabelId]["parent_id"];
+
+            if (curLabelId === labels[curLabelId]["root_id"]) {
+                if (Object.keys(labelCounter).includes(curLabelId)) {
+                    labelCounter[curLabelId].push(img["id"]);
+                } else {
+                    labelCounter[curLabelId] = [img["id"]];
+                }
+                break;
+            }
+        }
+    }
+    console.log(labelCounter);
+}
+
+const generateQuizzes = async(images, imageLabelRecords, labels) => {
+    const TOTAL_QUIZZES = 20;
+    const nineImages = getNineImages(images);
+    getDisplayLabel(nineImages, imageLabelRecords, labels);
 }
 
 // execution starts here
 getData().then(data => {
+    const images = data["images"];
     const imageLabelRecords = data["imageLabelRecords"];
     const labels = data["labels"];
-    const images = data["images"];
 
     // console.log("original: ", images);
-    // for (var i = 0; i < 1; ++i) {
-    //     const nineImages = getNineImages(images);
-    //     // assert(nineImages.length === 9);
-    //     console.log(nineImages.length);
-    //     console.log(`${i}: `, nineImages);
-    // }
-    // generateQuizzes(imageLabelRecords, labels);
+    // console.log(imageLabelRecords);
+    generateQuizzes(images, imageLabelRecords, labels);
 });
